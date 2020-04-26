df0f7e8c9e366f00ab3e686eb0bb457b37c5ed03
Alex Magalhaes <alex@pensatocode.org>
2020-04-26 11:14:13 +0200

    Java project moved to new repository

--- spring-simplicity-java-sample/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "spring-simplicity-java-sample"
--- spring-simplicity-java-sample/src/main/java/org/pensatocode/simplicity/sample/repository/CollegeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.pensatocode.simplicity.sample.repository;
-
-import org.pensatocode.simplicity.jdbc.JdbcRepository;
-import org.pensatocode.simplicity.sample.domain.College;
-
-public interface CollegeRepository extends JdbcRepository<College, Long> {
-}
